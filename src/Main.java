70e685e54d23df0e57b93b9a352814c4079e1014
Ashleigh Wong <ashleighewong@gmail.com>
2024-09-10 10:10:39 -0400

    added head notes for description

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+/**
+ * Solve the FizzBuzz challenge. Use do FizzBuzz method from FizzBuzz class.
+ */
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
