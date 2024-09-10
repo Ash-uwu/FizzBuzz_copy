e6fe3d52d280b518f1b4381fc8912294c11ede09
Ashleigh Wong <ashleighewong@gmail.com>
2024-09-10 10:23:49 -0400

    Completed class

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+/**
+ * Print out how many steps it takes to reach 0 if you start at 100.
+ */
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 2;
+        int steps = 0;
+
+        while (n != 0){
+            if (n % 2 == 0){
+                n = n / 2;
+                steps++;
+            }
+            else{
+                n--;
+                steps++;
+            }
+        }
+        System.out.println(steps);
+    }
 }
